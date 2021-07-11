6390b216afa3da26ec82ff49ce5061b3205e662e
jane96 <960447796@qq.com>
2021-07-11 22:23:58 +0800

    update first page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,35 @@
+from flask import render_template
 from app import app
 
-@app.route("/")
-@app.route("/index")
-def index():
-    return "this is the first test!"
+@app.route('/')
+@app.route('/first')
+def first():
+    user = { 'nickname': 'Miguel' } # fake user
+
+    return render_template("first.html",
+        title = 'Home',
+        user = user)
+@app.route('/second')
+def second():
+    user = { 'nickname': 'Miguel' } # fake user
+    return render_template("second.html",
+        title = 'Home',
+        user = user)
+@app.route('/thrid')
+def thrid():
+    user = { 'nickname': 'Miguel' } # fake user
+    return render_template("thrid.html",
+        title = 'Home',
+        user = user)
+@app.route('/four')
+def four():
+    user = { 'nickname': 'Miguel' } # fake user
+    return render_template("four.html",
+        title = 'Home',
+        user = user)
+@app.route('/five')
+def five():
+    user = { 'nickname': 'Miguel' } # fake user
+    return render_template("five.html",
+        title = 'Home',
+        user = user)
